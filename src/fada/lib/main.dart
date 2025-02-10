c45e66b2a2182b027b510eb209aa9d6d49cdbd9a
Tawaf Mesar <tawafmesar@gmail.com>
2025-02-10 08:23:34 +0300

    chore: load environment variables in main.dart

--- src/fada/lib/main.dart
+++ src/fada/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:fada/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:get/get.dart';
 import 'bindings/intialbindings.dart';
 import 'core/constant/routes.dart';
 import 'core/localization/changelocal.dart';
 import 'core/localization/translation.dart';
 import 'core/services/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialServices();
+  await dotenv.load(fileName: ".env");
 
   runApp(const MyApp());
 }
